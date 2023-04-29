836acc91382334bec10ff28ee7a64372b96fc55e
Bryan Andres <bry.andres001@gmail.com>
2023-04-29 11:31:17 -1000

    project get complete

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,43 @@
 // Write your "projects" router here!
+
+const express = require('express')
+const Project = require('./projects-model')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Project.get()
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'cant'
+            })
+        })
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const projects = await Project.get(id)
+        if (projects) {
+            res.status(200).json(projects)
+        } else {
+            res.status(404).json({
+                message: `no project with id: ${id}`
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'error'
+        })
+    }
+
+})
+
+
+
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const projectsRouter = require('./projects/projects-router');
 const server = express();
+
 
 // Configure your server here
 server.use(express.json())
+server.use('/api/projects', projectsRouter)
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!//
 
-server.get('/hello', (req, res) => {
-    res.status(200).json({ message: "hello world" })
-})
+
 
 module.exports = server;
