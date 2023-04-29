d0836efd876fb8f406541410174b3d65dcc6f6fa
Bryan Andres <bry.andres001@gmail.com>
2023-04-28 17:46:49 -1000

    first commit, connected

--- api/server.js
+++ api/server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const server = express();
 
 // Configure your server here
+server.use(express.json())
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+// Do NOT `server.listen()` inside this file!//
+
+server.get('/hello', (req, res) => {
+    res.status(200).json({ message: "hello world" })
+})
 
 module.exports = server;
