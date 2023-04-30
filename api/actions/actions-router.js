4e9861dbf39e479c841ba42e4ae1192772fcd97c
Bryan Andres <bry.andres001@gmail.com>
2023-04-30 00:55:08 -1000

    actions delete done

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -60,7 +60,7 @@ router.post('/', (req, res) => {
             .catch(err => {
                 res.status(500).json({
                     message: "error creating action",
-                    err: req.body,
+                    err: req.params,
                     stack: err.stack
 
                 })
@@ -102,6 +102,26 @@ router.put('/:id', (req, res) => {
                     stack: err.stack
                 })
             })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const action = await Actions.get(req.params.id)
+        if (!action) {
+            res.status(404).json({
+                message: "The action with specified id does not exist"
+            })
+        } else {
+            await Actions.remove(req.params.id)
+            res.json(action)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "This project could not be deleted",
+            err: err.message,
+            stack: err.stack
+        })
     }
 })
 
